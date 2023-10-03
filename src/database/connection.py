77c13b50d01461c614f5d3d031dc74c343693365
AntonHran <anton.hranovskyi@gmail.com>
2023-10-03 23:32:24 +0300

    database files

---
+++ src/database/connection.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException, status
+
+from src.conf.config import settings
+
+
+url = settings.sqlalchemy_database_url
+
+engine = create_engine(url, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    """
+    The get_db function is a context manager that returns the database session.
+    It also handles any exceptions that may occur during the session, and closes
+    the connection when it's done.
+
+    :return: A database session
+    :doc-author: Trelent
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    except SQLAlchemyError as error:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(error))
+    finally:
+        db.close()
