da9bbc0ae82bd584f76584c0996a9de644a13670
AntonHran <anton.hranovskyi@gmail.com>
2023-10-05 22:36:26 +0300

    files for a client

---
+++ client/javascript/app.js
@@ -0,0 +1,60 @@
+token = localStorage.getItem('accessToken')
+
+const get_cats = async () => {
+     const response = await fetch('http://localhost:8000/api/cats', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+     })
+    console.log(response.status, response.statusText)
+    if (response.status === 200) {
+        result = await response.json()
+        for (cat of result) {
+            el = document.createElement('li')
+            el.className = 'list-group-item'
+            el.innerHTML = `ID: ${cat.id} name: <b>${cat.nickname}</b> VaccinatedStatus: ${cat.vaccinated} Owner: ${cat.owner.email}`
+            cats.appendChild(el)
+        }
+    }
+}
+
+const get_owners = async () => {
+     const response = await fetch('http://localhost:8000/api/owners', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+     })
+    console.log(response.status, response.statusText)
+    if (response.status === 200) {
+        result = await response.json()
+        owners.innerHTML = ''
+        for (owner of result) {
+            el = document.createElement('li')
+            el.className = 'list-group-item'
+            el.innerHTML = `ID: ${owner.id} email: ${owner.email}`
+            owners.appendChild(el)
+        }
+    }
+}
+
+get_cats()
+get_owners()
+
+ownerCreate.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:8000/api/owners', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: ownerCreate.email.value
+        })
+    })
+    if (response.status === 201) {
+        console.log('Wners was created succesfully');
+        get_owners()
+    }
+})
