bd63be2b0e675a9f5aebb857cca5ecd7aa48ef81
AntonHran <anton.hranovskyi@gmail.com>
2023-10-08 03:30:28 +0300

    new fields for contact models were added

--- src/schemes/contacts.py
+++ src/schemes/contacts.py
@@ -7,18 +7,25 @@ from pydantic_extra_types.phone_numbers import PhoneNumber
 class ContactModel(BaseModel):
     first_name: str = Field(min_length=3, max_length=25)
     last_name: str = Field(min_length=4, max_length=30)
+    location: str | None = Field(min_length=0)
+    company: str | None = Field(min_length=0)
+    position: str | None = Field(min_length=0)
     email: EmailStr
     phone_number: PhoneNumber
     birth_date: date = None
-    notes: str
+    notes: str | None
 
 
 class ContactResponse(BaseModel):
     id: int = 1
     first_name: str
     last_name: str
+    location: str | None
+    company: str | None
+    position: str | None
+    photo: str | None
     email: EmailStr
     phone_number: PhoneNumber
-    birth_date: date = None
+    birth_date: date | None
     notes: str
     model_config = ConfigDict(from_attributes=True)
