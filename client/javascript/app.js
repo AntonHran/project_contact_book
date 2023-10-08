d93ecc64eb7f8599fd4973c752e2a5024b5f0a56
AntonHran <anton.hranovskyi@gmail.com>
2023-10-09 02:05:06 +0300

    new function was added

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -1,119 +1,223 @@
-const token = localStorage.getItem('accessToken')
+const token = localStorage.getItem("accessToken");
+let first_contact_id = localStorage.getItem("first_contact_id");
 
 const get_contacts = async () => {
-     const response = await fetch('http://localhost:8000/api/contacts', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        },
-     });
-    console.log(response.status, response.statusText)
-    if (response.status === 200) {
-        const result = await response.json()
-        const contactsList = document.getElementById('contactsList')
-        contactsList.innerHTML = '';
-        for (let contact of result) {
-            // Створюємо новий елемент списку (li) для кожного контакту
-            let contactElement = document.createElement("li");
-            contactElement.className = 'contact';
+  const response = await fetch("http://localhost:8000/api/contacts", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log(response.status, response.statusText);
+  if (response.status === 200) {
+    const result = await response.json();
+    const contactsList = document.getElementById("contactsList");
+    //localStorage.setItem("first_contact_id", result[0].id)
+    contactsList.innerHTML = "";
+    for (let contact of result) {
+      // Створюємо новий елемент списку (li) для кожного контакту
+      let contactElement = document.createElement("li");
+      contactElement.className = "contact";
 
-            // Створюємо контейнер для зображення контакту
-            let imageContainer = document.createElement("div");
-            imageContainer.className = 'contact-image';
+      contactElement.addEventListener("click", () => {
+        // Отримати ID контакту і передати його до функції displayContactData
+        const contactId = contact.id;
+        get_contact(contactId);
+      });
+      const contactElements = document.querySelectorAll(".contact");
+      contactElements.forEach((contactElement) => {
+        contactElement.addEventListener("click", () => {
+          // Скасовуємо виділення всіх інших елементів
+          contactElements.forEach((otherElement) => {
+            otherElement.style.backgroundColor = "";
+          });
 
-            // Створюємо зображення і додаємо його до контейнера
-            let imgElement = document.createElement("img");
-            imgElement.src = contact.photo;
-            imgElement.alt = "Фото контакту";
-            imgElement.className = 'contactImage';
-            imageContainer.appendChild(imgElement);
+          // Виділяємо обраний елемент
+          contactElement.style.backgroundColor = "#15B2F5";
+        });
+      });
 
-            // Створюємо контейнер для деталей контакту
-            let detailsContainer = document.createElement("div");
-            detailsContainer.className = 'contact-details';
+      // Створюємо контейнер для зображення контакту
+      let imageContainer = document.createElement("div");
+      imageContainer.className = "contact-image";
 
-            // Додаємо ім'я контакту та ID
-            let nameElement = document.createElement("h3");
-            nameElement.textContent = `${contact.first_name} ${contact.last_name}`;
-            nameElement.className = 'text-contact';
-            // let idElement = document.createElement("p");
-            // idElement.textContent = `ID: ${contact.id}`;
+      // Створюємо зображення і додаємо його до контейнера
+      let imgElement = document.createElement("img");
+      if (contact.photo) {
+        imgElement.src = contact.photo;
+      } else {
+        imgElement.src = "./images/contact_icon.jpg";
+      }
+      imgElement.alt = "Фото контакту";
+      imgElement.className = "contactImage";
+      imageContainer.appendChild(imgElement);
 
-            // Додаємо всі елементи до контейнера деталей
-            detailsContainer.appendChild(nameElement);
-            // detailsContainer.appendChild(idElement);
+      // Створюємо контейнер для деталей контакту
+      let detailsContainer = document.createElement("div");
+      detailsContainer.className = "contact-details";
 
-            // Додаємо контейнери до елемента списку
-            contactElement.appendChild(imageContainer);
-            contactElement.appendChild(detailsContainer);
+      // Додаємо ім'я контакту та ID
+      let nameElement = document.createElement("h3");
+      nameElement.textContent = `${contact.first_name} ${contact.last_name}`;
+      nameElement.className = "text-contact";
+      // let idElement = document.createElement("p");
+      // idElement.textContent = `ID: ${contact.id}`;
 
-            // Додаємо елемент контакту до списку
-            contactsList.appendChild(contactElement);
-            };
-        };
+      // Додаємо всі елементи до контейнера деталей
+      detailsContainer.appendChild(nameElement);
+      // detailsContainer.appendChild(idElement);
+
+      // Додаємо контейнери до елемента списку
+      contactElement.appendChild(imageContainer);
+      contactElement.appendChild(detailsContainer);
+
+      // Додаємо елемент контакту до списку
+      contactsList.appendChild(contactElement);
+    }
+  }
 };
 
-
-function addOption(selectbox,text,value, image)
-{var optn = document.createElement("OPTION");
-    optn.image = image
-    optn.text = text;
-    optn.value = value;
-    selectbox.options.add(optn);
-}
-
-get_contacts()
-// get_owners()
-
-contactCreate.addEventListener('submit', async (e) => {
-    e.preventDefault()
-    const response = await fetch('http://localhost:8000/api/contacts', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-            first_name: contactCreate.first_name.value,
-            last_name: contactCreate.last_name.value,
-            location: contactCreate.location.value,
-            company: contactCreate.company.value,
-            position: contactCreate.position.value,
-            email: contactCreate.email.value,
-            phone_number: contactCreate.phone.value,
-            birth_date: contactCreate.birthday.value,
-            notes: contactCreate.note.value,
-        })
-    })
-    console.log(response.body)
-    if (response.status === 201) {
-        console.log('Contact was created succesfully');
-        get_contacts()
+const get_contact = async (contact_id) => {
+  console.log(contact_id);
+  const response = await fetch(
+    `http://localhost:8000/api/contacts/${contact_id}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-})
+  );
+  console.log(response.status, response.statusText);
+  if (response.status === 200) {
+    const result = await response.json();
+    const contact = document.getElementById("contact-id");
+    contact.innerHTML = "";
+    //for (let field of result) {
+    // Створюємо новий елемент списку (li) для кожного поля контакта
+    let fieldElement = document.createElement("li");
+    fieldElement.className = "field";
 
+    // Створюємо контейнер для зображення поля
+    let imageContainer = document.createElement("div");
+    imageContainer.className = "field-image";
+
+    // Створюємо зображення і додаємо його до контейнера
+    let imgElement = document.createElement("img");
+    if (result.photo) {
+      imgElement.src = result.photo;
+    } else {
+      imgElement.src = "./images/contact_icon.jpg";
+    }
+    imgElement.alt = "Фото контакту";
+    imgElement.className = "fieldImage";
+    imageContainer.appendChild(imgElement);
+
+    // Створюємо контейнер для деталей контакту
+    let detailsContainer = document.createElement("div");
+    detailsContainer.className = "field-details";
+
+    let pContainer = document.createElement("div");
+    pContainer.className = "p-field-details";
+
+    // Додаємо ім'я контакту та ID
+    let nameElement = document.createElement("h3");
+    nameElement.textContent = `${result.first_name} ${result.last_name}`;
+    nameElement.className = "text-contact";
+    let locationElement = document.createElement("p");
+    locationElement.className = "field-info-location";
+    locationElement.textContent = `Location: ${result.location}`;
+    let companyElement = document.createElement("p");
+    companyElement.className = "field-info-company";
+    companyElement.textContent = `Company: ${result.company}`;
+    let positionElement = document.createElement("p");
+    positionElement.className = "field-info-position";
+    positionElement.textContent = `Position: ${result.position}`;
+    let emailElement = document.createElement("p");
+    emailElement.className = "field-info-email";
+    emailElement.textContent = `Email: ${result.email}`;
+    let phoneElement = document.createElement("p");
+    phoneElement.className = "field-info-tel";
+    phoneElement.textContent = `Phone number: ${result.phone_number}`;
+    let birthdayElement = document.createElement("p");
+    birthdayElement.className = "field-info-birthday";
+    birthdayElement.textContent = `Birth date: ${result.birth_date}`;
+    let notesElement = document.createElement("p");
+    notesElement.className = "field-info-note";
+    notesElement.textContent = `Note: ${result.notes}`;
+
+    // Додаємо всі елементи до контейнера деталей
+    detailsContainer.appendChild(nameElement);
+    pContainer.appendChild(locationElement);
+    pContainer.appendChild(companyElement);
+    pContainer.appendChild(positionElement);
+    pContainer.appendChild(emailElement);
+    pContainer.appendChild(phoneElement);
+    pContainer.appendChild(birthdayElement);
+    pContainer.appendChild(notesElement);
+
+    // Додаємо контейнери до елемента списку
+    fieldElement.appendChild(imageContainer);
+    fieldElement.appendChild(detailsContainer);
+    fieldElement.appendChild(pContainer);
+
+    // Додаємо елемент контакту до списку
+    contact.appendChild(fieldElement);
+    //};
+  }
+};
+
+get_contacts();
+//get_contact(first_contact_id)
+
+contactCreate.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const response = await fetch("http://localhost:8000/api/contacts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      first_name: contactCreate.first_name.value,
+      last_name: contactCreate.last_name.value,
+      location: contactCreate.location.value,
+      company: contactCreate.company.value,
+      position: contactCreate.position.value,
+      email: contactCreate.email.value,
+      phone_number: contactCreate.phone.value,
+      birth_date: contactCreate.birthday.value,
+      notes: contactCreate.note.value,
+    }),
+  });
+  console.log(response.body);
+  if (response.status === 201) {
+    console.log("Contact was created succesfully");
+    get_contacts();
+  }
+});
 
 const get_owners = async () => {
-     const response = await fetch('http://localhost:8000/api/owners', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        },
-     })
-    console.log(response.status, response.statusText)
-    if (response.status === 200) {
-        result = await response.json()
-        owners.innerHTML = ''
-        for (owner of result) {
-            el = document.createElement('li')
-            el.className = 'list-group-item'
-            el.innerHTML = `ID: ${owner.id} email: ${owner.email}`
-            owners.appendChild(el)
-        }
+  const response = await fetch("http://localhost:8000/api/owners", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log(response.status, response.statusText);
+  if (response.status === 200) {
+    result = await response.json();
+    owners.innerHTML = "";
+    for (owner of result) {
+      el = document.createElement("li");
+      el.className = "list-group-item";
+      el.innerHTML = `ID: ${owner.id} email: ${owner.email}`;
+      owners.appendChild(el);
     }
-}
+  }
+};
 
- (() => {
+(() => {
   const refs = {
     openModalBtn: document.querySelector("[data-create-open]"),
     closeModalBtn: document.querySelector("[data-create-close]"),
