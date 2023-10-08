ba0b4e616d255f5c40fb6aa3cd8922c42cdfb33e
AntonHran <anton.hranovskyi@gmail.com>
2023-10-08 20:26:28 +0300

    expire time was increased to 150 minutes

--- src/services/auth.py
+++ src/services/auth.py
@@ -36,7 +36,7 @@ class Token:
         if expires_delta:
             expire = datetime.utcnow() + timedelta(seconds=expires_delta)
         else:
-            expire = datetime.utcnow() + timedelta(minutes=15)
+            expire = datetime.utcnow() + timedelta(minutes=150)
         to_encode.update(
             {"iat": datetime.utcnow(), "exp": expire, "scope": "access_token"}
         )
