c1334f125f0e8cf47aa06a7948d7b1cb378e39a6
AntonHran <anton.hranovskyi@gmail.com>
2023-10-08 20:26:00 +0300

    function get_contacts was rewrite

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -1,4 +1,4 @@
-token = localStorage.getItem('accessToken')
+const token = localStorage.getItem('accessToken')
 
 const get_contacts = async () => {
      const response = await fetch('http://localhost:8000/api/contacts', {
@@ -6,29 +6,61 @@ const get_contacts = async () => {
         headers: {
             'Authorization': `Bearer ${token}`,
         },
-     })
+     });
     console.log(response.status, response.statusText)
     if (response.status === 200) {
-        result = await response.json()
-        contacts.innerHTML = ''
-        for (contact of result) {
-            el = document.createElement('li')
-            el.className = 'list-group-item'
-            el.innerHTML = `ID: ${contact.id} First name: <b>${contact.first_name}</b>
-                            Last name: ${contact.last_name} <br> Location: ${contact.location} <br>
-                            Company: ${contact.company} <br> Position: ${contact.position}
-                            Photo: ${contact.photo} <br> Email: ${contact.email} <br>
-                            Phone number: ${contact.phone_number} <br> Birthday: ${contact.birth_date} <br>
-                            Notes: ${contact.notes}`
-            el_1 = document.createElement("img")
-            el_1.className = 'image'
-            el_1.src = contact.photo
-            contacts.appendChild(el)
-            contacts.appendChild(el_1)
-        }
-    }
-}
+        const result = await response.json()
+        const contactsList = document.getElementById('contactsList')
+        contactsList.innerHTML = '';
+        for (let contact of result) {
+            // Створюємо новий елемент списку (li) для кожного контакту
+            let contactElement = document.createElement("li");
+            contactElement.className = 'contact';
 
+            // Створюємо контейнер для зображення контакту
+            let imageContainer = document.createElement("div");
+            imageContainer.className = 'contact-image';
+
+            // Створюємо зображення і додаємо його до контейнера
+            let imgElement = document.createElement("img");
+            imgElement.src = contact.photo;
+            imgElement.alt = "Фото контакту";
+            imgElement.className = 'contactImage';
+            imageContainer.appendChild(imgElement);
+
+            // Створюємо контейнер для деталей контакту
+            let detailsContainer = document.createElement("div");
+            detailsContainer.className = 'contact-details';
+
+            // Додаємо ім'я контакту та ID
+            let nameElement = document.createElement("h3");
+            nameElement.textContent = `${contact.first_name} ${contact.last_name}`;
+            nameElement.className = 'text-contact';
+            // let idElement = document.createElement("p");
+            // idElement.textContent = `ID: ${contact.id}`;
+
+            // Додаємо всі елементи до контейнера деталей
+            detailsContainer.appendChild(nameElement);
+            // detailsContainer.appendChild(idElement);
+
+            // Додаємо контейнери до елемента списку
+            contactElement.appendChild(imageContainer);
+            contactElement.appendChild(detailsContainer);
+
+            // Додаємо елемент контакту до списку
+            contactsList.appendChild(contactElement);
+            };
+        };
+};
+
+
+function addOption(selectbox,text,value, image)
+{var optn = document.createElement("OPTION");
+    optn.image = image
+    optn.text = text;
+    optn.value = value;
+    selectbox.options.add(optn);
+}
 
 get_contacts()
 // get_owners()
@@ -80,3 +112,32 @@ const get_owners = async () => {
         }
     }
 }
+
+ (() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-create-open]"),
+    closeModalBtn: document.querySelector("[data-create-close]"),
+    modal: document.querySelector("[data-create]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+//let el_1 = document.createElement("img");
+//            el_1.className = 'image';
+//            el_1.src = contact.photo;
+//            let el = document.createElement('theListBox');
+//            el.className = 'selectedItem';
+//            el.innerHTML = `ID: ${contact.id} Full name: <b>${contact.first_name} ${contact.last_name}</b>`;
+//                            //<br> Location: ${contact.location} <br>
+//                            //Company: ${contact.company} <br> Position: ${contact.position}
+//                            //Photo: ${contact.photo} <br> Email: ${contact.email} <br>
+//                            //Phone number: ${contact.phone_number} <br> Birthday: ${contact.birth_date} <br>
+//                            //Notes: ${contact.notes}
+//            contacts.appendChild(el_1);
+//            contacts.appendChild(el);
