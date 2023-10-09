72cbcb73caf120ed6dc4f320ceac6931e85ce1a3
AntonHran <anton.hranovskyi@gmail.com>
2023-10-10 01:21:05 +0300

    docstrings were added

--- src/routes/users.py
+++ src/routes/users.py
@@ -28,6 +28,14 @@ allowed_remove = RoleAccess([Role.admin])
 async def read_users_me(current_user: User = Depends(auth_user.get_current_user),
                         db: Session = Depends(get_db)):
 
+    """
+The read_users_me function returns the current user's information.
+
+    :param current_user: User: Get the current user
+    :param db: Session: Pass the database session to the function
+    :return: A user object
+    :doc-author: Trelent
+    """
     return await repository_users.get_user_by_id(current_user.id, db)
 
 
@@ -59,6 +67,17 @@ async def update_avatar_user(file: UploadFile = File(),
 async def get_users(limit: int = Query(10, le=100),
                     offset: int = 0,
                     db: Session = Depends(get_db)):
+
+    """
+    The get_users function returns a list of users.
+
+    :param limit: int: Limit the number of users returned
+    :param le: Limit the maximum number of users that can be returned
+    :param offset: int: Skip a number of records
+    :param db: Session: Pass the database session to the function
+    :return: A list of users
+    :doc-author: Trelent
+    """
     return await repository_users.get_all_users(limit, offset, db)
 
 
@@ -66,14 +85,35 @@ async def get_users(limit: int = Query(10, le=100),
             dependencies=[Depends(allowed_get)],
             description="For moderators and admin only")
 async def get_user(user_id: int = Path(ge=1), db: Session = Depends(get_db)):
+
+    """
+    The get_user function returns a user by id.
+        Args:
+            user_id (int): The id of the user to return.
+            db (Session, optional): SQLAlchemy Session. Defaults to Depends(get_db).
+
+    :param user_id: int: Specify the type of data that is expected to be passed in
+    :param db: Session: Pass the database session to the function
+    :return: A user object
+    :doc-author: Trelent
+    """
     return await repository_users.get_user_by_id(user_id, db)
 
 
-@router.delete("/{user_id", response_model=UserResponse,
+@router.delete("/{user_id}", response_model=UserResponse,
                dependencies=[Depends(allowed_remove)],
                description="For admin only")
 async def remove_user(user_id: int = Path(ge=1),
                       db: Session = Depends(get_db)):
+
+    """
+    The remove_user function removes a user from the database.
+
+    :param user_id: int: Specify the user id of the user to be deleted
+    :param db: Session: Pass the database session to the repository function
+    :return: The user that was removed
+    :doc-author: Trelent
+    """
     user = await repository_users.remove_user(user_id, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
